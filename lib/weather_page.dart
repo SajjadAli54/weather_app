db7b1a97ee2b9506f995459e9a4c2bcf8b3533be
Sajjad Ali <khaskhelisajjadali6@gmail.com>
2024-08-16 23:27:27 +0500

    Add current city and temp UI

--- lib/weather_page.dart
+++ lib/weather_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:weather_app/widgets/search_field.dart';
 
 class WeatherPage extends StatefulWidget {
@@ -20,11 +21,46 @@ class _WeatherPageState extends State<WeatherPage> {
             height: double.infinity,
             fit: BoxFit.cover,
           ),
-          const Padding(
-            padding: EdgeInsets.all(10.0),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
             child: SingleChildScrollView(
               child: Column(
-                children: [SizedBox(height: 40), SearchField()],
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 40),
+                  const SearchField(),
+                  const SizedBox(height: 20),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.location_on,
+                      ),
+                      Text(
+                        "Karachi",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      )
+                    ],
+                  ),
+                  const Text(
+                    "39.7 C",
+                    style: TextStyle(fontSize: 90, fontWeight: FontWeight.bold),
+                  ),
+                  Row(
+                    children: [
+                      const Text("Haze",
+                          style: TextStyle(
+                              fontSize: 40, fontWeight: FontWeight.w500)),
+                      Image.asset(
+                        "assets/icons/haze.png",
+                        height: 80,
+                      )
+                    ],
+                  )
+                ],
               ),
             ),
           )
